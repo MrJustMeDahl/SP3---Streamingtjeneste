b0542bc3c87de3bef5ffe5c211e8c4ee94741fdd
CarstenJuhl <cph-cj505@cphbusiness.dk>
2022-11-07 22:20:49 +0100

    StartMenu done
    StartMenu done

--- src/StartMenu.java
+++ src/StartMenu.java
@@ -1,84 +1,94 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*
-Make usefull Constructors
-Fix User problem, createNewUser should return a user
-Make a checkUserData
-Make a saveUserData
- */
-
 
 public class StartMenu {
-  private static final Scanner scanner = new Scanner(System.in);
-  String currentUser;
+ArrayList<User> allUsers = new ArrayList<User>();
+Scanner scanner = new Scanner(System.in);
 
- StartMenu (ArrayList<User> AllUsers){
+public StartMenu(ArrayList<User> allUsers){
+this.allUsers = allUsers;
+
 
 }
-
+//******
 //StartMenu Choose to login or create new user
-public String runStartMenu(String currentUser){
+//******
+public User runStartMenu(){
   System.out.println("1 - Login");
   System.out.println("2 - Create new user");
   while (true){
     String answer = scanner.nextLine();
     if ("1".equals(answer)){
-      return this.login();
+      return login();
     }
     if ("2".equals (answer)){
-      return this.makeNewUser();
+      return createNewUser();
     }
     System.out.println("The key you have pressed doesn't exist");
     System.out.println("Try again: ");
   }
-  ProgramControl.setCurrentUser = currentUser;
-}
+  User dummyuser = runStartMenu();
 
-//Set user, else return to loginscreen
-private String login(){
+  return dummyuser;
+}
+//******
+//Set user, or return to loginscreen
+//******
+private User login(){
   System.out.println("Please enter your Username:");
   System.out.println("or press 'q' to quit");
   String name = scanner.nextLine();
-  if (name != ProgramControl.allUsers){
-    System.out.println("This User doesn't exists. ");
-  }
-  else if (name.equalsIgnoreCase("q")) {
-    runStartMenu(currentUser);
-  }
-  System.out.println("Please enter your Password: ");
-  System.out.println("or press 'q' to quit");
-  String Password = scanner.nextLine();
-  if (Password != ProgramControl.allUsers){
-    System.out.println("you have entered the wrong Password: ");
-  }
-  else if (name.equalsIgnoreCase("q")) {
-    runStartMenu(currentUser);
-  }
-return currentUser;
+ for(User u:allUsers){
+   if (u.getName.equals(name) ) {
+     System.out.println("Please enter your Password: ");
+     String Password = scanner.nextLine();
+     if (Password.equalsIgnoreCase("q")) {
+       name = "q";
+       break;
+     }
+     if (u.getPassword.equals(Password)){
+       return u;
+     }
+     System.out.println("you have entered an unknown Password: ");
+   }
 
+ }
+  if (name.equalsIgnoreCase("q")) {
+  runStartMenu();
+  }
+  System.out.println("This User doesn't exists. ");
+ User dummyuser = runStartMenu();
+
+return dummyuser;
 }
+
+
+//******
 //Create a new user
-private makeNewUser createNewUser(makeNewUser aNewUser){
-  System.out.println("Please enter a Username: ");
-  String userName = scanner.nextLine();
-  System.out.println("Please enter a Password: ");
-  String passWord = scanner.nextLine();
-  System.out.println("Please enter your age: ");
-  int age = scanner.nextInt();
+//******
+private User createNewUser(){
+   int age = -1;
+   String userName;
+   String password;
 
-  return aNewUser;
-  ProgramControl.allUsers.add(aNewUser);
-}
+   while (true) {
+     System.out.println("Please enter a Username: ");
+     userName = scanner.nextLine();
+     System.out.println("Please enter a Password: ");
+     password = scanner.nextLine();
+     System.out.println("Please enter your age: ");
+     try {
+       age = Integer.parseInt(scanner.nextLine());
+       if (age != -1){
+         break;
+       }
+     } catch (Exception e) {
+       System.out.println("Age not registered");
 
-private String checkUserData(){
+     }
+     }
+     return new User(userName, password, age, new ArrayList<AMedia>(), new ArrayList<AMedia>());
 
-
-}
-
-private String saveUserData(){
-
-
-}
-
+   }
 }
