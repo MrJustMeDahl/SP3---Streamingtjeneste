ffefdb0b11a2e372197eb6683bcbbf5686a1c3d6
DanyLoyal <cph-dk174@cphbusiness.dk>
2022-11-08 13:34:50 +0100

    ive created the abstract class AMedia and the classes that extends from it. Movie.java, Series.java

--- src/Movie.java
+++ src/Movie.java
@@ -3,4 +3,14 @@ public class Movie extends AMedia {
     Movie(String name, String category, float rating, int releaseYear) {
         super(name, category, rating, releaseYear);
     }
+
+    @Override
+    protected void playMedia(AMedia media) {
+        if (ProgramControl.currentUser.getSavedMedia().contains(media)){
+            ProgramControl.currentUser.removeSavedMedia(media);
+        }
+        ProgramControl.currentUser.addWatchedMedia(media);
+        System.out.println("You are now watching : " + name);
+
+    }
 }
